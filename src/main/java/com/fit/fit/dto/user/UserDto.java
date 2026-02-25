9ba096be4a6dafbd6e35ceed8ecf071ba463e82e
box03 <box03-853@yandex.ru>
2026-02-25 13:33:51 +0400

    Написал класс UserDto и начал писать UserService

---
+++ src/main/java/com/fit/fit/dto/user/UserDto.java
@@ -0,0 +1,37 @@
+package com.fit.fit.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import com.fit.fit.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder// Узнать для чего нужны три ниже аннотации
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+    private Integer id;
+    private String userName;
+    private String email;
+    private String password;
+    // поле для записи времени регистрации
+    private LocalDateTime createdAt;
+    // поле для записи времени изменения пользвователя
+    private LocalDateTime updatedAt;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.userName = user.getUserName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
+}
---
+++ src/main/java/com/fit/fit/service/UserService.java
@@ -0,0 +1,13 @@
+package com.fit.fit.service;
+
+import com.fit.fit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+
+}
